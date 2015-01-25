429bf416f9d94bc2270f70865aaf0eeeb8d083a3
lorenzop <lorenzo@poixson.com>
2015-01-25 06:00:56 -0500

    added support for building projects without -SNAPSHOT in the version in sedVersion() function

--- pxn/build_utils.sh
+++ pxn/build_utils.sh
@@ -83,6 +83,8 @@ fi
 sedVersion() {
 	sed -i.original "s/x-SNAPSHOT/${BUILD_NUMBER}-SNAPSHOT/" "${1}" \
 		|| { echo "Failed to sed a file! ${1}"; return 1; }
+	sed -i "s/x-\</version\>/${BUILD_NUMBER}-\</version\>/" "${1}" \
+		|| { echo "Failed to sed a file! ${1}"; return 1; }
 }
 restoreSed() {
 	mv -fv "${1}.original" "${1}" \
