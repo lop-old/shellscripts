4c7437061ebd8327122be11e5ea9f08f00c38143
lorenzop <lorenzo@poixson.com>
2015-06-11 15:55:53 -0400

    added z alias (to view wz without watch)

--- pxn/aliases.sh
+++ pxn/aliases.sh
@@ -96,7 +96,8 @@ alias cdu='clear;du -sch *'
 alias du1='du -h --max-depth=1'
 
 
-alias wz='watch -d -n 2 "zpool iostat -v | sed \\"/^\s*$/d\\" ; zpool status | sed \\"/^\s*$/d\\" | grep -v errors\\:\\ No\\ known\\ data\\ errors ; echo ; df -h ; zfs get compressratio | grep --invert-match --color=none 1.00"'
+alias z='zpool iostat -v | sed \\"/^\s*$/d\\" ; zpool status | sed \\"/^\s*$/d\\" | grep -v errors\\:\\ No\\ known\\ data\\ errors ; echo ; df -h ; zfs get compressratio | grep --invert-match --color=none 1.00'
+alias wz='watch -d -n 2 "z"'
 #alias wz='watch "zpool iostat -v;zpool status;echo;df -h;zfs get compressratio|grep --invert-match --color=none 1.00"'
 alias bmdisk='time dd if=/dev/zero of=$PWD/test.file bs=1M count=10000;ll $PWD/test.file;rm $PWD/test.file'
 alias syncmem='sync;echo 3 > /proc/sys/vm/drop_caches'
