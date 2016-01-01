4b518c149ce1bfd9666ee1c367cfb9706f29453d
lorenzop <lorenzo@poixson.com>
2015-12-31 19:23:22 -0500

    added valid_ip() function to common.sh

--- src/common.sh
+++ src/common.sh
@@ -188,6 +188,23 @@ function get_lock() {
 
 
 
+# function valid_ip {
+#	local ip=$1
+#	local stat=1
+#	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+#		OIFS=$IFS
+#		IFS='.'
+#		ip=($ip)
+#		IFS=$OIFS
+#		[[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+#		&& ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+#		stat=$?
+#	fi
+#	return $stat
+# }
+
+
+
 function rsync_backup {
 	if [ -z "${1}" ] || [ -z "${2}" ]; then
 		errcho 'Source and destination arguments are required'
